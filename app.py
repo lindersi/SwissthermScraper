ec8012a121ec4b5cf4e472cf83fc03fa0a9f9f82
Simon <76875781+lindersi@users.noreply.github.com>
2022-01-07 18:35:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ for abrufversuche in range(5):  # Anzahl Versuche im Fehlerfall
         #    raise TimeoutError('Test')  # zum Testen des Fehlerfalls
 
         #  Betriebsdaten Heizkreisübersicht
-        driver.get(secrets.datapath)
+        driver.get(secrets.portal_datapath)
 
         element = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, 'div.overlay'))
